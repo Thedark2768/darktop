dabb0e8dcb6ac2812c47c0c95ae4d8fa65f5ef43
Thedark2768 <109007983+Thedark2768@users.noreply.github.com>
2026-07-31 21:26:00 -0600

    Set up Firebase configuration and Firestore
    Initialize Firebase app and Firestore database.

---
+++ scripts/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhFO0vj9KVVs5dRKpfritpHPl3y6_HY3A",
+  authDomain: "darktop-94a0a.firebaseapp.com",
+  projectId: "darktop-94a0a",
+  storageBucket: "darktop-94a0a.firebasestorage.app",
+  messagingSenderId: "468740111278",
+  appId: "1:468740111278:web:d2600ffdefccdb7238e63f"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
