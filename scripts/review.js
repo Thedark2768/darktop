6306ad24502cbe259de2dd78c070834962e7f230
Thedark2768 <109007983+Thedark2768@users.noreply.github.com>
2026-08-05 18:59:29 -0600

    Enhance review code validation on button click
    Refactor validateButton click event to include asynchronous code validation for review codes.

--- scripts/review.js
+++ scripts/review.js
@@ -1,7 +1,47 @@
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
 const validateButton = document.getElementById("validateButton");
 
-validateButton.addEventListener("click", () => {
+const reviewCode = document.getElementById("reviewCode");
 
-    console.log("Botón funcionando");
+const message = document.getElementById("message");
+
+validateButton.addEventListener("click", async () => {
+
+    const codigo = reviewCode.value.trim();
+
+    message.textContent = "";
+
+    if (codigo === "") {
+
+        message.textContent = "Introduce un código.";
+
+        return;
+
+    }
+
+    const consulta = query(
+        collection(db, "pedidos"),
+        where("codigoResena", "==", codigo)
+    );
+
+    const resultado = await getDocs(consulta);
+
+    if (resultado.empty) {
+
+        message.textContent = "❌ Código inválido.";
+
+        return;
+
+    }
+
+    message.textContent = "✅ Código encontrado.";
 
 });
